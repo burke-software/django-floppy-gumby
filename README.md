624d5388ce38f3ad653d84b073d64af3092f9064
David Burke <dmbst32@gmail.com>
2013-11-04 11:26:00 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+django-floppy-gumby
+===================
+
+[![Build Status](https://travis-ci.org/burke-software/django-content-edit.png?branch=master)](https://travis-ci.org/burke-software/django-content-edit)
+
+![ScreenShot](/images/screen.png)
+
+# Setup
+
+1. pip install django-floppy-gumby`
+1. Add `'gumby_floppy_forms',` to INSTALLED_APPS
+1. Set up [django-floppy-forms](http://django-floppyforms.readthedocs.org/)
+
---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "django-floppy-gumby",
+    version = "0.1",
+    author = "David Burke",
+    author_email = "david@burkesoftware.com",
+    description = ("Gumby Framework forms for Django using floppy forms. A floppy and gumby project."),
+    license = "BSD",
+    keywords = "django gumby",
+    url = "https://github.com/burke-software/django-floppy-gumby",
+    packages=find_packages(),
+    include_package_data=True,
+    test_suite='setuptest.setuptest.SetupTestSuite',
+    tests_require=(
+        'django-setuptest',
+    ),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Programming Language :: Python',
+        'Intended Audience :: Developers',
+        "License :: OSI Approved :: BSD License",
+    ],
+    install_requires=['django', 'django-floppyform']
+)
